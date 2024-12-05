c1c9d497c2c736ec55ae4dbb8b2777c795c629f7
germain <germaindev123@gmail.com>
2024-12-05 16:05:33 +0100

    add echo function

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,28 @@ fn main() {
     }
 }
 
-fn handle_command(command: &str) {
+fn handle_command(input: &str) {
+
+    /// split the line received into a vector, with the command in the index 0
+    let split = input.split_whitespace().collect::<Vec<_>>();
+    let command = split[0];
+    let args = split[1..].to_vec();
+
     match command.trim().to_lowercase().as_str() {
-        "exit 0" => std::process::exit(0),
-        &_ => { println!("{}: command not found", command); }
+        "exit" => exit(),
+        "echo" => echo(args.join(" ").as_str()),
+        &_ => invalid_command(command)
     }
+}
+
+fn exit(){
+    std::process::exit(0);
+}
+
+fn echo(arg: &str) {
+    println!("{}", arg);
+}
+
+fn invalid_command(command: &str) {
+    eprintln!("{}: command not found", command);
 }
