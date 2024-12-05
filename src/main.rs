fc36a21307b9d245d7aef74fee17485a3d56ca02
germain <e207384s>
2024-12-05 19:18:37 +0100

    add Path to type command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::fs::read_dir;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::{Path, PathBuf};
+use std::env::*;
+
+const PROMPT: &str = "$ ";
+const BUILTINS: [&str; 3] = ["echo", "type", "exit"];
 
 fn main() {
     // Wait for user input
@@ -18,10 +24,10 @@ fn main() {
 
 fn handle_command(input: &str) {
 
-    /// split the line received into a vector, with the command in the index 0
-    let split = input.split_whitespace().collect::<Vec<_>>();
-    let command = split[0];
-    let args = split[1..].to_vec();
+    // split the line received into a vector, with the command in the index 0
+    let tokens = input.split_whitespace().collect::<Vec<_>>();
+    let command = tokens[0]; // get the command
+    let args = tokens[1..].to_vec();
 
     match command.trim().to_lowercase().as_str() {
         "exit" => exit(),
@@ -43,9 +49,40 @@ fn invalid_command(command: &str) {
     eprintln!("{}: command not found", command);
 }
 
+
+/// the type command is used to determine the type of command
 fn type_cmd(command: &str) {
-    match command {
-        "type" | "echo" | "exit"  => println!("{} is a shell builtin", command),
-        &_ => {println!("{}: not found", command);}
+
+    // check if the command is a shell builtin
+    if BUILTINS.contains(&command) {
+        println!("{} is a shell builtin", command);
+        return;
     }
+
+    let paths = find_command_in_path(command);
+
+    if paths.len() > 0 {
+        for path in paths {
+            println!("{}", path);
+        }
+        return;
+    }
+
+    eprintln!("{}: command not found", command);
+}
+
+fn find_command_in_path(command: &str) -> Vec<String> {
+    let line = var("PATH").unwrap();
+    let paths = split_paths(&line);
+    let mut found_paths = Vec::new();
+
+    for path in paths {
+        for entry in path.read_dir().expect("read_dir failed"){
+            match entry {
+                Ok(e) => if e.file_name() == command {  found_paths.push(String::from(path.to_str().unwrap()))}
+                Err(_) => {}
+            }
+        }
+    }
+    found_paths
 }
